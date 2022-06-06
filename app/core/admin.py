f534d1284f0c792c3590981e9484cc6fefc78587
EldarGazizov <kursus1010@gmail.com>
2022-06-06 22:41:39 +0200

    django admin custom user show and test

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,37 @@
-from django.contrib import admin  # Noqa
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as _
 
-# Register your models here.
+from .models import User
+
+
+class UserAdmin(BaseUserAdmin):
+    ordering = ["id"]
+    list_display = ["email", "name"]
+    fieldsets = [
+        [None, {"fields": ["email", "password"]}],
+        [
+            _("Permissions"),
+            {
+                "fields": [
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                ]
+            },
+        ],
+        [_("Important dates"), {"fields": ["last_login"]}],
+    ]
+    readonly_fields = ["last_login"]
+    add_fieldsets = [
+        [
+            None,
+            {
+                "classes": ["wide"],
+                "fields": ["email", "password1", "password2", "name", "is_active", "is_staff", "is_superuser"],
+            },
+        ]
+    ]
+
+
+admin.site.register(User, UserAdmin)
