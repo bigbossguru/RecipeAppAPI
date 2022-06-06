94cd7e58951dc8f868591ca2d5f1282caa180c3c
EldarGazizov <kursus1010@gmail.com>
2022-06-06 23:28:00 +0200

    API documentation swagger

--- app/app/settings.py
+++ app/app/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    "drf_spectacular",
     "core",
 ]
 
@@ -136,3 +137,5 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = "core.User"
+
+REST_FRAMEWORK = {"DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema"}
